520823205856b997b0a55fc9d3d1ea9751746a2b
M. Irfan Hidayat <irfanhidayatms@outlook.com>
2019-06-10 16:11:00 +0700

    initial commit

---
+++ PluginMediaExample/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PluginMediaExample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            button.Clicked += Button_Clicked;
+
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable && !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", "No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(
+                new StoreCameraMediaOptions
+                {
+                    DefaultCamera = CameraDevice.Rear,
+                    SaveToAlbum = true,
+                    Name = "test.jpg"
+                });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+
+        }
+    }
+}
